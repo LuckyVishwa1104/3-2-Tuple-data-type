7de55826f3fa598ea27ef6f83c1df36fcc4a90f2
Lucky <luckyvishwa1104@gmail.com>
2023-05-14 12:10:02 +0530

    modified and structured the first file

--- 1_Operations on Tuple/1_Creating_tuple.py
+++ 1_Operations on Tuple/1_Creating_tuple.py
@@ -1,28 +1,34 @@
-#2. Tuple data-type - collection of elem. separated by comma in braces.
+#2. Tuple data-type : tuple is another collection data type, it consist of element of similar or different data type.
+# element are enclosed within circle brackets and separeted by commas.
+# tuple are generally immutable i.e. they can't be modified or changed.
 # Tuple = (val1, val2, val3, - - - ,valn)
 
-#1]. Creating and Declaring a list
+#1]. Creating and Declaring a Tuple 
 ''' Syntax :
 	    tuple_name=(val1, val2, - - - , valn) '''
 ''' Tuple - Variable (A to Z, a to z, 0 to 9, _)
- Values - all data-type, variables, input,                        expression '''	    
+ Values - all data-type, variables, input, expression '''	
+
 # even a tuple contain single value comma is used to separate i.e. t=(66,)	    
 #1. tuple of similar datatype
 tuple1=(23,5,79,87,9,86) 
 print(tuple1)  # tuple of intigers
 tuple2=("hii","lol","oop","kdk","hily")
 print(tuple2)  # tuple of string
+
 #2. tuple of mixed datatype
 tuple3=(-3,6,7.2,-6.7,9-7j,0b101,0o45,0xA2E)
 print(tuple3)
 tuple4=(7,9.2,"strin",bool(88),False,None,True)
 print(tuple4)
+
 #3. tuple of complex datatype
 a,b,c=5,7,9
 tuple5=(2,"uuo",a,8.3,b,-5-7j,c)
 print(tuple5)
 tuple6=(a*b,b+c,a//c,8-6)
 print(tuple6)
+
 #4. tuple of advanced datatype
 tuple7=("us",0xA4,[a,b,c],(3,6,9),{2:8,3:27,4:64},{a*b,58})
 print(tuple7)
